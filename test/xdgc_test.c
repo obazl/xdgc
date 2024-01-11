e98ce3963b319862527404a29b579d7f83aa9510
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2024-01-11 11:36:46 -0600

    merge dev

--- test/xdgc_test.c
+++ test/xdgc_test.c
@@ -13,25 +13,45 @@
 
 #include <unistd.h>
 
-#include "log.h"
-
+#include "liblogc.h"
 #include "utarray.h"
 #include "utstring.h"
 #include "utstring.h"
-
 #include "xdgc_test.h"
+
+extern char *xdgc_version;
+
+#if defined(PROFILE_fastbuild)
+#define TRACE_FLAG xdgc_trace
+bool TRACE_FLAG;
+#define DEBUG_LEVEL xdgc_debug
+int  DEBUG_LEVEL;
+#endif
+
+void usage(char *arg)
+{
+    log_info("Usage: %s [-f] [findlibfile]", arg);
+}
 
 int main(int argc, char *argv[])
 {
     int opt;
 
-    while ((opt = getopt(argc, argv, "hv")) != -1) {
+    while ((opt = getopt(argc, argv, "hdtv")) != -1) {
         switch (opt) {
         case 'h':
-            log_info("Help: ");
+            usage(argv[0]);
             exit(EXIT_SUCCESS);
+#if defined(PROFILE_fastbuild)
+        case 'd':
+            xdgc_debug++;
+            break;
+        case 't':
+            xdgc_trace = true;
+            break;
+#endif
         default:
-            log_error("Usage: %s [-f] [findlibfile]", argv[0]);
+            usage(argv[0]);
             exit(EXIT_FAILURE);
         }
     }
@@ -39,15 +59,30 @@ int main(int argc, char *argv[])
     char *wd = getenv("BUILD_WORKING_DIRECTORY");
     if (wd) {
         /* we launched from bazel workspace, cd to launch dir */
-        chdir(wd);
+        int rc = chdir(wd);
+        (void)rc;
     }
 
-    char *s = xdg_data_home();
-    log_info("xdg data home: %s", s);
+    log_info("xdgc version %s", xdgc_version);
+
+    char *s = xdg_bin_home();
+    log_info("xdg bin home: %s", s);
+    free(s);
+
+    s = xdg_cache_home();
+    log_info("xdg cache home: %s", s);
     free(s);
 
     s = xdg_config_home();
     log_info("xdg config home: %s", s);
+    free(s);
+
+    s = xdg_data_home();
+    log_info("xdg data home: %s", s);
+    free(s);
+
+    s = xdg_state_home();
+    log_info("xdg state home: %s", s);
     free(s);
 
     log_info("exiting");
